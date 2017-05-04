2ea8b4166579ac6cce50a32bb011a0dadbf08532
Aayush Chanda <AChanda2@student.naperville203.org>
2017-05-04 10:31:00 -0500

    5/4/17

--- AIPaddle.java
+++ AIPaddle.java
@@ -3,8 +3,8 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.*;
 
-public class AIPaddle {
-
+public class AIPaddle 
+{
 	int x;
 	int y;
 	int width = 20;
@@ -19,7 +19,8 @@ public class AIPaddle {
 	boolean goingUp = false;
 	boolean goingDown = false;
 
-	public AIPaddle(int x, int y) {
+	public AIPaddle(int x, int y) 
+	{
 		this.x = x;
 		this.y = y;
 
@@ -27,29 +28,37 @@ public class AIPaddle {
 		boundingBox.setBounds(x, y, width, height);
 	}
 
-	public void tick(Game game) {
+	public void tick(Game game) 
+	{
 		boundingBox.setBounds(x, y, width, height);
 
-		if (!isTwoPlayer) {
-			if (game.ball.y < y && y >= 0) {
+		if (!isTwoPlayer) 
+		{
+			if (game.ball.y < y && y >= 0) 
+			{
 				y -= speed;
-			} else if (game.ball.y > y && y + height <= game.getHeight()) {
+			} 
+			else if (game.ball.y > y && y + height <= game.getHeight()) 
+			{
 				y += speed;
 			}
 		} 
-		else {
-			if (goingUp) {
+		else 
+		{
+			if (goingUp) 
+			{
 				y -= speed;
 			} 
-			else if (goingDown) {
+			else if (goingDown)
+			{
 				y += speed;
 			}
 		}
 	}
 
-	public void render(Graphics g) {
+	public void render(Graphics g) 
+	{
 		g.setColor(new Color(generator.nextInt(256), generator.nextInt(256), generator.nextInt(256)));
 		g.fillRect(x, y, width, height);
 	}
-
 }
